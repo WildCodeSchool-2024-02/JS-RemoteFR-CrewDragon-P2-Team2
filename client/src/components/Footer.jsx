df414d6542fc454f526aa5536a98e97176c7ecb6
ETCHART Amandine <amandine.yann.lola@gmail.com>
2024-04-12 09:28:49 +0200

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Harry Potter belongs to J.K. Rowling</p>
+      <p> Names ang images are the property of their authors</p>
+      <p>Developers: Wizards Coders</p>
+      <p>Game created in React JS</p>
+      <p>Duels in Hogwarts © 2024</p>
+    </div>
+  );
+}
+
+export default Footer;
