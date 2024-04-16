07e8a3c6be2b65c6e7805813e4ecd5b82654e791
David L <159317073+daviscrib@users.noreply.github.com>
2024-04-16 17:41:25 +0200

    Add background in playground

--- client/src/components/Playground.jsx
+++ client/src/components/Playground.jsx
@@ -1,8 +1,27 @@
 function Playground() {
   return (
-    <div className="playground">
-      <p>Hello world</p>
-    </div>
+    <>
+      <h2>Playground</h2>
+      <section className="containerChooseCharacterCard">
+        <article
+          className="playground"
+          style={{
+            background: `url('../src/assets/images/In the livingroom.webp')`,
+            backgroundPosition: "center",
+            backgroundRepeat: "noRepeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="sandbox">
+            <div className="text">
+              <div>round 1</div>
+              <div>harry sort 1 sort 2 draco</div>
+              <div>sort</div>
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
   );
 }
 
--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -1,5 +1,13 @@
 function ScoreBoard() {
-  return <h1>Hello React</h1>;
+  return (
+    <>
+      <h1>ScoreBoard</h1>
+      <p>Find your result here !</p>
+      <section className="containerChooseCharacterCard">
+        <p>Hello</p>
+      </section>
+    </>
+  );
 }
 
 export default ScoreBoard;
