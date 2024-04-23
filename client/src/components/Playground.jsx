aa8b0a4fcfc2503b21d92aa31a4a01b34d207637
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-04-23 14:27:12 +0200

    add style state for selectedSpell

--- client/src/components/Playground.jsx
+++ client/src/components/Playground.jsx
@@ -34,9 +34,10 @@ function Playground() {
       isUsedComputer: false,
     },
   ]);
-
-  const [displaySpell, setDisplaySpell] = useState("toto");
-  const [displayRandomSpell, setDisplayRandomSpell] = useState("tata");
+  // On affiche le sort du user et du computer
+  const [style, setStyle] = useState("");
+  const [displaySpell, setDisplaySpell] = useState("");
+  const [displayRandomSpell, setDisplayRandomSpell] = useState("");
 
   const handleClick = (selectedSpell) => {
     // on filtre le sort selectionné
@@ -44,19 +45,23 @@ function Playground() {
       spell.name === selectedSpell ? { ...spell, isUsed: true } : spell
     );
 
-    // on choisi un random pour l'adversaire
+    // on crée un tableau filtré pour choisir un random de l'adversaire
     const spellsFilter = updateSpells.filter(
       (spell) => spell.isUsedComputer === false
     );
+    // On trouve un sort random parmi les disponibles
     const randomSpell =
       spellsFilter[Math.floor(Math.random() * spellsFilter.length)];
+    // On rends le sort selectionné au hasard, plus utilisable
     randomSpell.isUsedComputer = true;
 
     setDisplayRandomSpell(randomSpell.name);
     setSpells(updateSpells);
     setDisplaySpell(selectedSpell);
+    setStyle("bg-dabrown");
   };
 
+  // On affiche la modal par défaut
   const [show, setShow] = useState(true);
   const handleModal = () => {
     setShow(!show);
@@ -80,12 +85,16 @@ function Playground() {
               <h3 className="titleRound">Round 1</h3>
               <article className="layoutPlayers">
                 <div className="player">
-                  <p className="btn-third">{displaySpell}</p>
+                  {displaySpell !== "" && (
+                    <p className={`btn-third ${style}`}>{displaySpell}</p>
+                  )}
                   <CharacterCard />
                 </div>
                 <div className="player">
                   <CharacterCard />
-                  <p className="btn-third">{displayRandomSpell}</p>
+                  {displayRandomSpell !== "" && (
+                    <p className="btn-third">{displayRandomSpell}</p>
+                  )}
                 </div>
                 <div className="nextButton">
                   <button type="button" className="btn-primary">
