2e1228726db68237af403083be646947689b7e11
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-04-25 22:53:09 +0200

    display card scoreboard

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -1,6 +1,7 @@
+import CharacterCardPlayer from "./CharacterCardPlayer";
+
 function ScoreBoard() {
   const results = JSON.parse(localStorage.getItem("results"));
-
   return (
     <section
       className="py-4 w-80 mx-auto flex flex-col items-center"
@@ -9,10 +10,18 @@ function ScoreBoard() {
       <h2 className="title-sections">ScoreBoard</h2>
       <p>Find your result here !</p>
       <article className="containerNeumorphism">
-        <p>
-          {results[0].player.name} {results[0].score < 0 ? "Lost " : "Win "}
-          again {results[0].computer.name}
-        </p>
+        {results[0].player.name !== "" && (
+          <>
+            <CharacterCardPlayer
+              fighter={results[0].player}
+              health={results[0].player.health}
+            />
+            <CharacterCardPlayer
+              fighter={results[0].computer}
+              health={results[0].computer.health}
+            />
+          </>
+        )}
       </article>
     </section>
   );
