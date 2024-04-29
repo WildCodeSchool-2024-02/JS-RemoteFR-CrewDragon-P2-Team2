bae38766028f7318559c92dce0774a0f85ae4deb
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-04-29 11:59:19 +0200

    hotfix scoreboard

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -1,43 +1,27 @@
 import CharacterCardPlayer from "./CharacterCardPlayer";
 
 function ScoreBoard() {
-  const results =
-    JSON.parse(localStorage.getItem("results")) === null
-      ? [
-          {
-            score: 0,
-            player: {
-              name: "",
-              image: "",
-              health: 100,
-            },
-            computer: {
-              name: "",
-              image: "",
-              health: 100,
-            },
-          },
-        ]
-      : JSON.parse(localStorage.getItem("results"));
+  const results = JSON.parse(localStorage.getItem("results"));
   return (
     <section
       className="py-4 w-80 mx-auto flex flex-col items-center"
       id="score_board"
     >
       <h2 className="title-sections">ScoreBoard</h2>
-      <p>Find your result here !</p>
+      <p>Find your last battle here</p>
       <article className="containerNeumorphism">
-        {results[0].player.name !== "" && (
-          <>
+        {results !== null && (
+          <div className="w-full flex lg:flex-row flex-col justify-around items-center gap-4 lg:justify-center">
             <CharacterCardPlayer
               fighter={results[0].player}
               health={results[0].player.health}
             />
+            <p>VS</p>
             <CharacterCardPlayer
               fighter={results[0].computer}
               health={results[0].computer.health}
             />
-          </>
+          </div>
         )}
       </article>
     </section>
